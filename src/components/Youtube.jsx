a8cad15118d2b282427761a200ca13fd9890faa0
Md Wasim Ansari <119097409+md-wasim-123@users.noreply.github.com>
2024-06-10 12:48:57 +0530

    Update Youtube.jsx iframe video change

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -6,8 +6,7 @@ const Youtube = () => {
     return (
         <div className='w-full h-auto bg-slate-600'>
             <div class="aspect-w-10 aspect-h-4">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/HXkQq61gtg4?si=joCnGSuakxgPmIv0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/7apj4sVvbro?si=v_zAu9Dfpuu038Px" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <div className='f-bg py-5 flex items-center justify-center gap-5 md:gap-10'>
                 <div className=' text-xl md:text-3xl font-bold text-white '>Connect with us !</div>
                 <div className='  flex items-center justify-center'>
